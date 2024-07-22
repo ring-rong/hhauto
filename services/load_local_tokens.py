88b24b01c875ed7a76e4ec40cf50ea88470a253c
ring-rong <132333846+ring-rong@users.noreply.github.com>
2024-07-23 01:39:21 +1000

    Update load_local_tokens.py

--- services/load_local_tokens.py
+++ services/load_local_tokens.py
@@ -1,13 +1,11 @@
 import json
-import os
 from .connecting import obj
 
+
 def load_tokens_auth():
-    xsrf = os.getenv('xsrf')
-    hhtoken = os.getenv('hhtoken')
-    
-    if xsrf and hhtoken:
-        obj.xsrf = xsrf
-        obj.hhtoken = hhtoken
-    else:
-        print("Tokens not found in environment variables.")
+    with open('config/tokens.json', 'r') as file:
+        data = json.load(file)
+
+    if data != {}:
+        obj.xsrf = data['xsrf']
+        obj.hhtoken = data['hhtoken']
